9e426a314759c5b8812471e8e7313ddba7681dce
Alibi Duisenaliyev <realibi.dev@gmail.com>
2022-11-17 20:17:37 +0600

    heal function added

--- alibi.js
+++ alibi.js
@@ -2,7 +2,18 @@ let hp = 100;
 let energy = 5;
 let coins = 500;
 
-function startBattle(){
-    let battleDiv = document.querySelector("#battle");
-    battleDiv.innerHTML += `<input type="number">`;
+const hpSpan = document.querySelector("#hp");
+const coinSpan = document.querySelector("#coins");
+const energySpan = document.querySelector("#energy");
+
+function heal(){
+    if(coins >= 50){
+        coins -= 50;
+        hp += 20;
+        
+        hpSpan.innerHTML = hp;
+        coinSpan.innerHTML = coins;
+    }else{
+        alert("Not enough coins!");
+    }
 }
