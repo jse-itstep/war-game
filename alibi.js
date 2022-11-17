1c80d05d0c7289691d8234727bb7e07b58fab554
Alibi Duisenaliyev <realibi.dev@gmail.com>
2022-11-17 21:13:16 +0600

    battle logic added

--- alibi.js
+++ alibi.js
@@ -17,3 +17,33 @@ function heal(){
         alert("Not enough coins!");
     }
 }
+
+function startBattle(){
+    if(energy > 0){
+        energy--;
+        let roundsCount = +prompt();
+
+        if(roundsCount >= 2){
+            for(let i = 0; i < roundsCount; i++){
+                hp -= Math.round(Math.random() * 30) + 10;
+            }
+    
+            if(hp > 0){
+                coins += roundsCount * 100;
+                alert("You win the battle!")
+
+                if(coins >= 1000){
+                    alert("You won the game!");
+                }
+            }else{
+                alert("You lose!");
+            }
+    
+            hpSpan.innerHTML = hp;
+            coinSpan.innerHTML = coins;
+            energySpan.innerHTML = energy;
+        }else{
+            alert("Minimun 2 fights!");
+        }
+    }
+}
