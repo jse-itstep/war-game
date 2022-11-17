a31d49715588e3c9abbc66338f458378c2bf2a24
YerdauletU <yerdauletu.03@gmail.com>
2022-11-17 21:04:23 +0600

    ssvsgvb

--- yerdaulet.js
+++ yerdaulet.js
@@ -7,11 +7,11 @@ let coins = document.querySelector("#coins");
 function heal() {
     let new_hp = +hp.innerText + 20;
     let new_coins = +coins.innerText;
-    if (+coins.innerText > 50) { 
+    if (+coins.innerText >= 50) { 
         coins.innerHTML = new_coins - 50;
+        hp.innerHTML = new_hp;
     }
     else {
         coins.innerHTML = 0;
     }
-    hp.innerHTML = new_hp;
 }   
