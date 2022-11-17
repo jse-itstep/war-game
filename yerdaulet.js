c91fd63a188fc038356a2d610a1a17ffd4954133
YerdauletU <yerdauletu.03@gmail.com>
2022-11-17 20:52:38 +0600

    sfgsgsg

--- yerdaulet.js
+++ yerdaulet.js
@@ -5,5 +5,13 @@ let energy = document.querySelector("#energy");
 let coins = document.querySelector("#coins");
 
 function heal() {
-    
+    let new_hp = +hp.innerText + 20;
+    let new_coins = +coins.innerText;
+    if (+coins.innerText > 50) { 
+        coins.innerHTML = new_coins - 50;
+    }
+    else {
+        coins.innerHTML = 0;
+    }
+    hp.innerHTML = new_hp;
 }
