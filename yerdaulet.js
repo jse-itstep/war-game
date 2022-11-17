71dcf175173664702d4c4d4eb204622b7acd3564
YerdauletU <yerdauletu.03@gmail.com>
2022-11-17 20:22:18 +0600

    sgsdbdrb

--- yerdaulet.js
+++ yerdaulet.js
@@ -1,1 +1,9 @@
-console.log("qweeerrr")
+console.log("qweeerrr");
+
+let hp = document.querySelector("#hp");
+let energy = document.querySelector("#energy");
+let coins = document.querySelector("#coins");
+
+function heal() {
+    
+}
