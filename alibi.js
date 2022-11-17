dc37f8a89ea1fc11c955bdfe46d927bc9325c8d3
Alibi Duisenaliyev <realibi.dev@gmail.com>
2022-11-17 20:08:35 +0600

    init

--- alibi.js
+++ alibi.js
@@ -1,1 +1,8 @@
-console.log("asd");
+let hp = 100;
+let energy = 5;
+let coins = 500;
+
+function startBattle(){
+    let battleDiv = document.querySelector("#battle");
+    battleDiv.innerHTML += `<input type="number">`;
+}
