e9b0cc1c9bc5f05a8240d668a85a5cc368675f1e
Еламан Сураншиев <@itstep.loc>
2022-11-17 20:48:33 +0600

    done

---
+++ yelaman.js
@@ -0,0 +1,24 @@
+let hp = 100;
+let energy = 5;
+let coins = 500;
+
+const hpSpan = document.querySelector("#hp");
+const coinSpan = document.querySelector("#coins");
+const energySpan = document.querySelector("#energy");
+
+function startBattle(){
+    let divBattle = document.querySelector("#battle");
+    divBattle.innerHTML += `<input type="text">`
+}
+
+
+function heal(){
+    if(coins >= 50){
+        coins -= 50;
+        hp += 20;
+        hpSpan.innerHTML = hp;
+        coinSpan.innerHTML = coins;
+    }else{
+        alert("Not enough coins!");
+    }
+}
